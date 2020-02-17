c8c48359f5b57e30c0c9149898f177b81fe1cf21
Mario Hammer <mario.hammer@logicline.de>
2020-02-17 22:53:29 +0100

    weather app

--- server.js
+++ server.js
@@ -2,18 +2,37 @@
 projectData = {};
 
 // Require Express to run server and routes
-
+const express = require('express');
+const bodyParser = require('body-parser')
+const cors = require('cors');
 // Start up an instance of app
-
+const app = express();
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-
+app.use(cors());
 // Cors for cross origin allowance
 
 // Initialize the main project folder
 app.use(express.static('website'));
 
-
 // Setup Server
+
+app.get('/all', (req, res) => {
+    res.send(projectData);
+});
+
+app.post('/data', (req, res) => {
+    console.log(req.body);
+    const d = Date.now();
+    projectData = req.body;
+    res.sendStatus(200);
+});
+
+app.delete('/data', (req, res) => {
+    projectData = {};
+    res.sendStatus(200);
+});
+
+app.listen(3000, () => console.log('Server started'));
--- website/app.js
+++ website/app.js
@@ -1,5 +1,71 @@
 /* Global Variables */
+const API_KEY = '6a24c42d694388188acea5c73afe1688';
+const BASE = 'http://api.openweathermap.org/data/2.5/weather';
+const SERVER = 'http://localhost:3000'
 
 // Create a new date instance dynamically with JS
-let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+
+document.addEventListener('DOMContentLoaded', e => {
+    const button = document.getElementById('generate');
+    button.addEventListener('click', createEntry);
+});
+
+function createEntry(e) {
+    getWeather()
+    .then(postResponse)
+    .then(getData)
+    .then(update);
+}
+
+async function update (result) {
+    console.log('Updateing UI: ' + JSON.stringify(result));
+    const date = document.getElementById('date');
+    const temp = document.getElementById('temp');
+    const content = document.getElementById('content');
+    const holder = document.getElementById('entryHolder');
+
+    const d = holder.getAttribute('display');
+    holder.setAttribute('display', 'none');
+
+    date.textContent = result.date;
+    temp.textContent = result.temperature;
+    content.textContent = result.user_response;
+
+    holder.setAttribute('display', d);
+}
+
+async function getData () {
+    const url = SERVER+'/all';
+    console.log(`Getting ${url}`);
+    const response = await fetch(url);
+    return await response.json();
+}
+
+async function postResponse (json) {
+    const url = SERVER+'/data';
+    console.log(`Posting ${JSON.stringify(json)} to ${url}`);
+    return await fetch(url, data = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+          },
+        body: JSON.stringify(json)
+    });
+}
+
+async function getWeather(event) {
+    const zip = document.getElementById('zip').value;
+    const feelings = document.getElementById('feelings').value;
+    const url =`${BASE}?zip=${zip},de&appid=${API_KEY}`;
+    console.log(`getting weather data: ${url}`);
+    const response = await fetch(url);
+    const json = await response.json();
+    const d = new Date();
+    const newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+
+    return {
+        temperature: json.main.temp,
+        date: d,
+        user_response: feelings
+    };
+}
